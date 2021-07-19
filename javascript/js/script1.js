02e90ad8f002f616ed44c54d43a8701e65c6fb93
Richard Silva <richardwillian166@hotmail.com>
2021-07-19 20:51:49 -0300

    Adicionando e manipulando objetos no Array

---
+++ javascript/js/script1.js
@@ -0,0 +1,57 @@
+class Produto{
+    
+    constructor(){
+        this.id = 1;
+        this.arrayProdutos= [];
+        
+    }
+
+    salvar(){
+        let produto = this.lerDados();
+
+        if(this.validaCampos(produto)){
+            alert('salvar')
+        }
+
+        console.log(this.arrayProdutos);
+        this.adicionar(produto)
+        
+    }
+
+    adicionar(produto){
+        this.arrayProdutos.push(produto)
+        this.id++
+    }
+    lerDados(){
+        let produto = {}
+
+        produto.id = this.id  
+        produto.nomeProduto= document.getElementById('produto').value
+        produto.preco = document.getElementById('preco').value
+
+        return produto;
+        
+    }
+
+    validaCampos(produto){
+        let msg='';
+
+        if (produto.nomeProduto ==''){
+        msg += ' - Informe o nome do produto \n'
+        }
+        if(produto.preco ==''){
+            msg += ' - Informe o nome do produto\n'
+        }
+
+        if (msg != ''){
+            alert(msg);
+            return false
+        }
+        return true;
+    }
+
+    cancelar(){
+        alert('item deletado');
+    }
+}
+var produto = new Produto();
